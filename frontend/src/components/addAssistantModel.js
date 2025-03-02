0670810b542a3836539e1d0b476820e1a41cd9b1
TharuHiru <hirusha-im21011@stu.kln.ac.lk>
2025-03-02 14:01:55 +0530

    Add assistant model UI updates

--- frontend/src/components/addAssistantModel.js
+++ frontend/src/components/addAssistantModel.js
@@ -5,6 +5,8 @@ import { Button, Modal, Form } from 'react-bootstrap';
 import axios from 'axios';
 import { toast } from 'react-toastify'; // Import Toastify for toast notifications
 import 'react-toastify/dist/ReactToastify.css';
+import { FaUser } from 'react-icons/fa'; // Import icons
+import '../Styles/loginForms.css';
 
 const AddAssistantModal = ({ showModal, handleClose }) => {
   const [assistantDetails, setAssistantDetails] = useState({
@@ -30,20 +32,12 @@ const AddAssistantModal = ({ showModal, handleClose }) => {
 
   // Form validation logic
   const validateForm = () => {
-    if (!assistantDetails.firstname.trim())  {
-      toast.error('First Name is required');
-      return false;
-    }
-    if (!assistantDetails.lastname.trim()) {
-      toast.error('Last Name is required');
-      return false;
-    }
-    if (!assistantDetails.email.trim()) {
-      toast.error('Email is required');
-      return false;
-    }
-    if (!assistantDetails.contact.trim()) {
-      toast.error('Contact number is required');
+    if ((!assistantDetails.firstname.trim()) 
+        || (!assistantDetails.lastname.trim())
+        || (!assistantDetails.email.trim())
+        || (!assistantDetails.contact.trim())) 
+    {    
+      toast.error('Please Fill all the values');
       return false;
     }
     return true;
@@ -75,28 +69,130 @@ const AddAssistantModal = ({ showModal, handleClose }) => {
   };
 
   return (
-    <Modal show={showModal} onHide={handleClose} backdrop="static" keyboard={false}>
+    <Modal show={showModal} onHide={handleClose} backdrop="static" keyboard={false} >
       <Modal.Header closeButton>
-        <Modal.Title>Add New Assistant</Modal.Title>
+      <Modal.Title className='addAssistTitle'>
+          <FaUser size={30}/> &nbsp;
+          Add New Assistant
+      </Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        <Form onSubmit={handleSubmit}>
-          {Object.entries(assistantDetails).map(([key, value]) => (
-            <Form.Group key={key} controlId={`form${key}`} className="mb-3">
-              <Form.Label>{key.charAt(0).toUpperCase() + key.slice(1)}</Form.Label>
-              <Form.Control
-                type={key === "password" ? "password" : "text"}
-                placeholder={`Enter ${key}`}
-                name={key}
-                value={value}
+      <Modal.Body  >
+      <Form onSubmit={handleSubmit} className='addAssistForm' >
+        <Form.Group controlId="formNIC" className='addAssisttextbox'>
+          <Form.Label>NIC</Form.Label>          
+            <Form.Control 
+                type="text"
+                placeholder="Enter NIC"
+                name="nic"
+                value={assistantDetails.nic}
                 onChange={handleInputChange}
-              />
-            </Form.Group>
-          ))}
-          <Button variant="primary" type="submit" disabled={loading}>
-            {loading ? 'Submitting...' : 'Submit'}
-          </Button>
-        </Form>
+        />
+        </Form.Group>
+
+        <Form.Group controlId="formTitle" className='addAssisttextbox'>
+          <Form.Label>Title</Form.Label>          
+            <Form.Control
+                type="text"
+                placeholder="Enter Title"
+                name="title"
+                value={assistantDetails.title}
+                onChange={handleInputChange}
+        />
+        </Form.Group>
+
+        <Form.Group controlId="formFirstName" className='addAssisttextbox'>
+          <Form.Label>First Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter first name"
+            name="firstName"
+            value={assistantDetails.firstname}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formLastName" className='addAssisttextbox'>
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter last name"
+            name="lastName"
+            value={assistantDetails.lastname}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formContact" className='addAssisttextbox'>
+            <Form.Label>Contact</Form.Label>
+            <Form.Control
+                type="text"
+                placeholder="Enter Contact"
+                name="contact"
+                value={assistantDetails.contact}
+                onChange={handleInputChange}
+            />
+        </Form.Group>
+
+        <Form.Group controlId="formHouseNo" className='addAssisttextbox'>
+            <Form.Label>House No</Form.Label>
+            <Form.Control
+                type="text"
+                placeholder="Enter House No"
+                name="houseNo"
+                value={assistantDetails.houseNo}
+                onChange={handleInputChange}
+            />
+        </Form.Group>
+
+        <Form.Group controlId="formAddLine1" className='addAssisttextbox'>
+            <Form.Label>Address Line 1</Form.Label>
+            <Form.Control
+                type="text"
+                placeholder="Enter Address Line 1"
+                name="addline1"
+                value={assistantDetails.addline1}
+                onChange={handleInputChange}
+            />
+        </Form.Group>
+
+        <Form.Group controlId="formAddLine2" className='addAssisttextbox'>
+            <Form.Label>Address Line 2</Form.Label>
+            <Form.Control
+                type="text"
+                placeholder="Enter Address Line 2"
+                name="addline2"
+                value={assistantDetails.addline2}
+                onChange={handleInputChange}
+            />      
+        </Form.Group>
+
+        <Form.Group controlId="formEmail" className='addAssisttextbox'>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            value={assistantDetails.email}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formPassword" className='addAssisttextbox'>   
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+                type="password"
+                placeholder="Enter password"
+                name="password"
+                value={assistantDetails.password}
+                onChange={handleInputChange}
+            />
+        </Form.Group>
+
+            <Button variant="primary" type="submit" disabled={loading} className="btn btn-primary w-100 loginBtn">
+              Add Assistant
+              {loading ? 'Submitting...' : ''}
+            </Button>
+          </Form>
       </Modal.Body>
     </Modal>
   );

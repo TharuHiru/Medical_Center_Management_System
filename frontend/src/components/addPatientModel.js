9095b5bb42f1874a2bf2a1a154c2c2490eceba68
TharuHiru <hirusha-im21011@stu.kln.ac.lk>
2025-03-04 15:19:54 +0530

    add patient form changed

--- frontend/src/components/addPatientModel.js
+++ frontend/src/components/addPatientModel.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from 'react';
-import { Button, Modal, Form } from 'react-bootstrap';;
+import { Button, Modal, Form, Row, Col } from 'react-bootstrap';
 import { toast } from 'react-toastify'; // Import Toastify for toast notifications
 import 'react-toastify/dist/ReactToastify.css';
 import { FaUser } from 'react-icons/fa'; // Import icons
@@ -74,122 +74,143 @@ const AddPatientModal = ({ showModal, handleClose }) => {
   };
 
   return (
-    <Modal show={showModal} onHide={handleClose} backdrop="static" keyboard={false} >
+    <Modal show={showModal} onHide={handleClose} backdrop="static" keyboard={false}>
       <Modal.Header closeButton>
-      <Modal.Title className='addAssistTitle'>
-          <FaUser size={30}/> &nbsp;
-          Add New Patient
-      </Modal.Title>
+        <Modal.Title className="addAssistTitle">
+          <FaUser size={30} /> &nbsp; Add New Patient
+        </Modal.Title>
       </Modal.Header>
-      <Modal.Body  >
-      <Form onSubmit={handleSubmit} className='addAssistForm' >
-        <Form.Group controlId="formNIC" className='addAssisttextbox'>
-          <Form.Label>Title</Form.Label>          
-            <Form.Control 
-                type="text"
-                placeholder="Enter Title"
-                name="title"
-                value={patientDetails.nic}
-                onChange={handleInputChange}
-        />
-        </Form.Group>
-
-        <Form.Group controlId="formTitle" className='addAssisttextbox'>
-          <Form.Label>Title</Form.Label>          
-            <Form.Control
-                type="text"
-                placeholder="Enter Title"
-                name="title"
-                value={patientDetails.title}
-                onChange={handleInputChange}
-        />
-        </Form.Group>
-
-        <Form.Group controlId="formFirstName" className='addAssisttextbox'>
-          <Form.Label>First Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter first name"
-            name="firstname"
-            value={patientDetails.firstname}
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-
-        <Form.Group controlId="formLastName" className='addAssisttextbox'>
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter last name"
-            name="lastname"
-            value={patientDetails.lastname}
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-
-        <Form.Group controlId="formContact" className='addAssisttextbox'>
-            <Form.Label>Contact</Form.Label>
-            <Form.Control
-                type="text"
-                placeholder="Enter Contact"
-                name="contact"
-                value={patientDetails.contact}
-                onChange={handleInputChange}
-            />
-        </Form.Group>
-
-        <Form.Group controlId="formHouseNo" className='addAssisttextbox'>
-            <Form.Label>House No</Form.Label>
-            <Form.Control
-                type="text"
-                placeholder="Enter House No"
-                name="houseNo"
-                value={patientDetails.houseNo}
-                onChange={handleInputChange}
-            />
-        </Form.Group>
-
-        <Form.Group controlId="formAddLine1" className='addAssisttextbox'>
-            <Form.Label>Address Line 1</Form.Label>
-            <Form.Control
-                type="text"
-                placeholder="Enter Address Line 1"
-                name="addline1"
-                value={patientDetails.addline1}
-                onChange={handleInputChange}
-            />
-        </Form.Group>
-
-        <Form.Group controlId="formAddLine2" className='addAssisttextbox'>
-            <Form.Label>Address Line 2</Form.Label>
-            <Form.Control
-                type="text"
-                placeholder="Enter Address Line 2"
-                name="addline2"
-                value={patientDetails.addline2}
-                onChange={handleInputChange}
-            />      
-        </Form.Group>
-
-        <Form.Group controlId="formEmail" className='addAssisttextbox'>
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            name="email"
-            value={patientDetails.email}
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-
-            <Button variant="primary" type="submit" disabled={loading} className="btn btn-primary w-100 loginBtn">
-              Add Patient
-              {loading ? 'Submitting...' : ''}
-            </Button>
-          </Form>
+  
+      <Modal.Body>
+        <Form onSubmit={handleSubmit} className="addAssistForm">
+          <Row className="mb-3">
+            <Col md={6}>
+              <Form.Group controlId="formTitle" className="formGroup">
+                <Form.Label>Title</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Title"
+                  name="title"
+                  value={patientDetails.title}
+                  onChange={handleInputChange}
+                  className="formControl"
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group controlId="formFirstName" className="formGroup">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter First Name"
+                  name="firstname"
+                  value={patientDetails.firstname}
+                  onChange={handleInputChange}
+                  className="formControl"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+  
+          <Row className="mb-3">
+            <Col md={6}>
+              <Form.Group controlId="formLastName" className="formGroup">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Last Name"
+                  name="lastname"
+                  value={patientDetails.lastname}
+                  onChange={handleInputChange}
+                  className="formControl"
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group controlId="formContact" className="formGroup">
+                <Form.Label>Contact</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Contact"
+                  name="contact"
+                  value={patientDetails.contact}
+                  onChange={handleInputChange}
+                  className="formControl"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+  
+          <Row className="mb-3">
+            <Col md={6}>
+              <Form.Group controlId="formHouseNo" className="formGroup">
+                <Form.Label>House No</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter House No"
+                  name="houseNo"
+                  value={patientDetails.houseNo}
+                  onChange={handleInputChange}
+                  className="formControl"
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group controlId="formAddLine1" className="formGroup">
+                <Form.Label>Address Line 1</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Address Line 1"
+                  name="addline1"
+                  value={patientDetails.addline1}
+                  onChange={handleInputChange}
+                  className="formControl"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+  
+          <Row className="mb-3">
+            <Col md={6}>
+              <Form.Group controlId="formAddLine2" className="formGroup">
+                <Form.Label>Address Line 2</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Address Line 2"
+                  name="addline2"
+                  value={patientDetails.addline2}
+                  onChange={handleInputChange}
+                  className="formControl"
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group controlId="formEmail" className="formGroup">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter Email"
+                  name="email"
+                  value={patientDetails.email}
+                  onChange={handleInputChange}
+                  className="formControl"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+  
+          <Button
+            variant="primary"
+            type="submit"
+            disabled={loading}
+            className="btn btn-primary w-100 loginBtn"
+          >
+            {loading ? 'Submitting...' : 'Add Patient'}
+          </Button>
+        </Form>
       </Modal.Body>
     </Modal>
   );
-};
+}  
 
 export default AddPatientModal;

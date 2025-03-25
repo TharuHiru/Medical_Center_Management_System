55cbc0f3de8f8b534a0680f400eb62094eeea438
TharuHiru <hirusha-im21011@stu.kln.ac.lk>
2025-03-25 15:30:10 +0530

    added firebase

---
+++ Backend/config/firebase.js
@@ -0,0 +1,20 @@
+// config/firebase.js
+import firebase from "firebase/app";
+import "firebase/firestore"; // Import Firestore
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const fdb = firebase.firestore();
+
+export { fdb };
